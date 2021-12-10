63c220bd95242e588c45d8aa275eb800992a8481
rivari22 <muchammadrvr22@gmail.com>
2021-12-10 21:05:57 +0700

    feat: react navigation and a few screens

---
+++ src/screens/ListScheduleScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ListScheduleScreen = () => {
+  return (
+    <View>
+      <Text>List Schedule</Text>
+    </View>
+  );
+};
+
+export default ListScheduleScreen;
